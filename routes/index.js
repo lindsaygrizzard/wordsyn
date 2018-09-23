beedff1f74e9edb5c7a7ccf9ae87cc10230d0a6d
Lindsay Grizzard <lindsay.grizzard@us-lgrizzard1.attlocal.net>
2018-09-23 13:46:30 -0700

    simple api up and running

---
+++ routes/index.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/search/:word', getSimWords)
+
+function getSimWords(req, res, next) {
+  const datamuse = require('datamuse');
+  var data = req.params;
+  var word = data.word;
+
+  datamuse.request('words?ml='+word)
+    .then((json) => {
+
+    console.log(json);
+
+    var reply;
+    // var parsedSimWord = JSON.parse(json)
+
+    for(i=0; i <= 30; i++){
+        reply += json[i].word + " ";
+    }
+      
+    // response.json(reply) 
+    
+    
+    res.render('index', {test: reply, title: "TEST TITLE"});
+      
+      
+  });
+
+
+  // res.render('index', { title: word });
+};
+
+
+
+module.exports = router;
